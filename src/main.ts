8cdb4dc0eada46568ea42895f0b2741b28ec671b
Kenny <101839505+khui0@users.noreply.github.com>
2026-02-12 21:39:10 -0500

    feat: add base express app

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("hi");
+import express from "express";
+
+process.loadEnvFile();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+export default app;
